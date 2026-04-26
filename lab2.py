fdb42e5e63e82c4759463e510dc4d53b40038ed7
Shaza-Elshimy <shazahamdy011@gmail.com>
2026-04-26 16:23:06 +0300

    add structured output commit

--- lab2.py
+++ lab2.py
@@ -3,29 +3,51 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
 from langchain.messages import HumanMessage
+from pydantic import BaseModel
+from typing import List
 load_dotenv()
 
 api_key=os.getenv("OPENAI_API_KEY")
 # print(api_key)
+class Ingredient(BaseModel):
+    name: str
+    status: str  # available / not available
 
-system_prompt="""
-You are a professional chef
+class Meal(BaseModel):
+    name: str
+    cooking_time: str
+    number_of_individuals: int
+    instructions: str
+    ingredients: List[Ingredient]
+
+class ChefResponse(BaseModel):
+    meals: List[Meal]
+
+
+system_prompt = """
+You are a professional chef.
 
 Rules:
-- Always think step-by-step
-- Analyze ingredients
-- Suggest meals clearly
-- Be friendly and human-like
+- Always analyze ingredients step-by-step
+- Never skip steps
+- Suggest multiple meals
+- Mark ingredients as available or not available
+- Be human-like and friendly
+
+IMPORTANT:
+Return ONLY structured output in JSON format matching schema.
 """
 
-llm= ChatOpenAI(
+structured_llm= ChatOpenAI(
     model="gpt-4o-mini",
     temperature=0.7,
     api_key=api_key
 )
+
 agent = create_agent(
-    llm,
-    system_prompt=system_prompt  
+    structured_llm,
+    system_prompt=system_prompt  ,
+    response_format=ChefResponse
 )
 
 res1 =agent.invoke({
@@ -39,7 +61,7 @@ res1 =agent.invoke({
     }
     ) 
 print("\n first respose: \n")
-print(res1["messages"][-1].content)
+print(res1['messages'][-1].content)
 
 res2=agent.invoke({
     "messages":[
@@ -53,4 +75,4 @@ res2=agent.invoke({
     ) 
 
 print("\n second respose: \n")
-print(res2["messages"][-1].content)
+print(res2['messages'][-1].content)
