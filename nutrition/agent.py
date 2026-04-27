465c3f944d3bdc030d046d54cf6b1ba90da09619
Shaza-Elshimy <shazahamdy011@gmail.com>
2026-04-28 00:02:53 +0300

    add search healthy place and store in CSV tools and enhance system prompt

--- nutrition/agent.py
+++ nutrition/agent.py
@@ -5,23 +5,49 @@ from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage
 from langgraph.checkpoint.memory import InMemorySaver
 
+from tavily import TavilyClient
+from langchain.tools import tool
+import csv
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
-
+tavily_key=os.getenv("TAVILY_KEY")
 
 system_prompt = """
 You are a Nutrition AI Assistant.
 
-- Analyze meals from text or image
-- Estimate calories and nutrients
-- Suggest improvements
+You must:
+- Analyze food (text or image)
+- Estimate calories
+- Suggest healthy alternatives
+- Use tools when needed:
+  - search healthy places
+  - store analysis in CSV
 
-Always include:
-"This is not medical or dietary advice. Consult a qualified professional."
+Always include disclaimer:
+"This is not medical or dietary advice.
 """
 
+@tool
+def search_healthy_places(meal: str, city: str) -> str:
+    """Search for healthy restaurants based on meal and city"""
+    client = TavilyClient()
+
+    query = f"healthy restaurants or meals for {meal} in {city}"
+    result = client.search(query)
+
+    return str(result)
 
 
+
+@tool
+def store_nutrition(meal: str, calories: str, summary: str) -> str:
+    """Store nutrition analysis in CSV"""
+
+    with open("nutrition_data.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([meal, calories, summary])
+
+    return "Saved successfully"
 llm = ChatOpenAI(
     model="gpt-4o-mini",
     temperature=0.7,
@@ -31,5 +57,6 @@ llm = ChatOpenAI(
 nutrition_agent = create_agent(
     llm,
     system_prompt=system_prompt,
+    tools=[search_healthy_places],
     checkpointer=InMemorySaver()
 )
