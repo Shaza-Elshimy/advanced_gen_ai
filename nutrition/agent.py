a5221756879991c465b3b5365d38c5ea834a34e3
Shaza-Elshimy <shazahamdy011@gmail.com>
2026-04-27 23:06:11 +0300

    create base nutrition agent with checkpoint memory

---
+++ nutrition/agent.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
+from langgraph.checkpoint.memory import InMemorySaver
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+
+system_prompt = """
+You are a Nutrition AI Assistant.
+
+- Analyze meals from text or image
+- Estimate calories and nutrients
+- Suggest improvements
+
+Always include:
+"This is not medical or dietary advice. Consult a qualified professional."
+"""
+
+
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0.7,
+    api_key=api_key
+)
+
+nutrition_agent = create_agent(
+    llm,
+    system_prompt=system_prompt,
+    checkpointer=InMemorySaver()
+)
