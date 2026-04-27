a0dcd9aec82d993dec2643a66f3f6241a4578b23
Shaza-Elshimy <shazahamdy011@gmail.com>
2026-04-27 23:12:56 +0300

    connect nutrition agent to ui (text input)

--- nutrition/views.py
+++ nutrition/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
 # Create your views here.
+from .agent import nutrition_agent
+from langchain.messages import HumanMessage
+
 def chat(request):
-    return render(request,"nutrition/chat.html")
+
+    response = None
+
+    if request.method == "POST":
+        meal = request.POST.get("meal")
+
+        res = nutrition_agent.invoke(
+            {
+                "messages": [
+                    HumanMessage(content=meal)
+                ]
+            },
+            config={"configurable": {"thread_id": "nutrition"}}
+        )
+
+        response = res["messages"][-1].content
+
+    return render(request, "nutrition/chat.html", {
+        "response": response
+    })
