4e9dd821074865728cf4b9af4cf4866ec3468f82
Shaza-Elshimy <shazahamdy011@gmail.com>
2026-04-30 21:13:47 +0300

    add Course Modael and add 3 courses

--- study_agent/models.py
+++ study_agent/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    instructor = models.CharField(max_length=200)
+    duration = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
